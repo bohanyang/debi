0c700551c2195accde18012aef7b83dc744eeef8
Bohan Yang <youthdna@live.com>
2021-01-15 10:10:42 +0000

    Update

--- debi.sh
+++ debi.sh
@@ -440,7 +440,7 @@ EOF
         echo "d-i partman-auto/disk string $disk" | $save_preseed
     else
         # shellcheck disable=SC2016
-        echo 'd-i partman/early_command string debconf-set partman-auto/disk "$(list-devices disk | head -n1 )"'
+        echo 'd-i partman/early_command string debconf-set partman-auto/disk "$(list-devices disk | head -n 1)"'
     fi
 
     [ "$force_gpt" = true ] && echo 'd-i partman-partitioning/default_label string gpt' | $save_preseed
