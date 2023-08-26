005754ffe8bed289918f4451dce026c6aa591b8b
Bohan Yang <brent@manyou.io>
2023-08-26 09:31:01 +0000

    Fix #103

--- debi.sh
+++ debi.sh
@@ -111,10 +111,10 @@ set_mirror_proxy() {
 
 set_security_archive() {
     case $suite in
-        buster|oldoldstable|bullseye|oldstable)
+        buster|oldoldstable)
             security_archive="$suite/updates"
             ;;
-        bookworm|stable|trixie|testing)
+        bullseye|oldstable|bookworm|stable|trixie|testing)
             security_archive="$suite-security"
             ;;
         sid|unstable)
@@ -502,15 +502,6 @@ done
 [ -n "$authorized_keys_url" ] && ! download "$authorized_keys_url" /dev/null &&
 err "Failed to download SSH authorized public keys from \"$authorized_keys_url\""
 
-non_free_firmware_available=false
-case $suite in
-    bookworm|stable|trixie|testing|sid|unstable)
-        non_free_firmware_available=true
-        ;;
-    *)
-        apt_non_free_firmware=false
-esac
-
 apt_components=main
 [ "$apt_contrib" = true ] && apt_components="$apt_components contrib"
 [ "$apt_non_free" = true ] && apt_components="$apt_components non-free"
@@ -780,13 +771,12 @@ $save_preseed << EOF
 
 # Apt setup
 
+d-i apt-setup/non-free-firmware boolean $apt_non_free_firmware
 d-i apt-setup/contrib boolean $apt_contrib
 d-i apt-setup/non-free boolean $apt_non_free
 d-i apt-setup/enable-source-repositories boolean $apt_src
 d-i apt-setup/services-select multiselect $apt_services
 EOF
-
-[ "$non_free_firmware_available" = true ] && echo "d-i apt-setup/non-free-firmware boolean $apt_non_free_firmware" | $save_preseed
 
 # If not sid/unstable
 [ -n "$security_archive" ] && {
